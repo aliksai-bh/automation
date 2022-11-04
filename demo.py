5ef58a2b3c2f6746e0eb5d35ff46d68960629d51
aliksai-bh <saidlikhon.alikhonov@bakerhughes.com>
2022-11-04 09:45:39 +0100

    trying

--- demo.py
+++ demo.py
@@ -4,13 +4,11 @@ from selenium.webdriver.common.by import By
 from time import sleep
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import FirefoxOptions
-import json
+
+#visiting the uptime kuma
 
 class Automation:
-    __opts = FirefoxOptions()
-    __opts.add_argument("--headless")
-    __driver = webdriver.Firefox(executable_path=r"./geckodriver",options=__opts)
-
+    __driver = webdriver.Chrome(executable_path=r"./chromedriver.exe")
     def __init__(self,url,user_name,password):
         self.url = url
         self.user_name = user_name
@@ -67,8 +65,7 @@ def main():
     password = "bh.admin"
     service_name = "test"
     service_url = "apple.com"
-    payload = json.loads(os.environ['PAYLOAD'])
-    print(f'url-{payload['url']} and name: {payload['website']}')
+
     process = Automation(url,login,password)
     process.login()
     process.set_notification()
@@ -77,3 +74,59 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+# try:
+#     driver.get("http://10.79.85.55:3001/add")
+# except:
+#     print("there was an error in opening the page")
+# sleep(3)
+# ### LOGIN ###
+# try:
+#     login = driver.find_element("id","floatingInput")
+#     password = driver.find_element("id","floatingPassword")
+#     login_btn = driver.find_element("class name","btn")
+#     login.send_keys('admin')
+#     password.send_keys('bh.admin')
+#     login_btn.click()
+#     print("login phase - successful")
+# except:
+#     print("there was an error in login phase")
+
+# ### ADDING WEBSITE ###
+# sleep(2)
+# try:
+#     notification = driver.find_element("id","notification1")
+#     notification.click()
+#     print("notification is turned on")
+# except:
+#     print("failed in notification phase")
+# #giving the name 
+# try:
+#     name_field = driver.find_element("id","name")
+#     name_field.send_keys("test name")
+#     print("naming phase - successful")
+# except:
+#     print('there was an error in naming phase')
+
+# #adding url
+# try: 
+#     url_field = driver.find_element("id","url")
+#     url_field.send_keys("apple.com")
+#     print("url adding phase - successful")
+# except:
+#     print('there was an error in url adding phase')
+
+# #saving
+# #scroll
+# try:
+#     driver.find_element(By.TAG_NAME,"html").send_keys(Keys.END)
+#     sleep(2)
+#     save_btn = driver.find_element("xpath",'//*[@id="monitor-submit-btn"]')
+#     save_btn.click()
+#     print("saving phase - successful")
+# except:
+#     print('there was an error in saving phase')
+# driver.quit()
+
